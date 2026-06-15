d51e17089f8b62bd56846c823bd13bfea768b5ec
Shreya Pandey <pshreya653@gmail.com>
2026-06-15 21:34:54 +0530

    chore: bootstrap spring boot backend

---
+++ src/test/java/com/pockit/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pockit.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
