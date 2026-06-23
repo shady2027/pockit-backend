bb8085147b6e5703fa1cffb4f1df6a20a32a6222
Shreya Pandey <pshreya653@gmail.com>
2026-06-23 08:49:49 +0530

    changes

--- src/main/java/com/pockit/PockitApplication.java
+++ src/main/java/com/pockit/PockitApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PockitApplication {
 
-	//main application class
 	public static void main(String[] args) {
 		SpringApplication.run(PockitApplication.class, args);
 	}
